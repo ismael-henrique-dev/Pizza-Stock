a86dddb7bc23f93aaa4cf56fe8f348c504b57d38
Ismael <ismael.henrique.dev@gmail.com>
2025-01-04 10:37:09 -0300

    feat: create register view

--- src/Controllers/App.java
+++ src/Controllers/App.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 /**
  * JavaFX App
  */
@@ -17,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 1920, 1080);
+        scene = new Scene(loadFXML("primary"), 680, 480);
         stage.setScene(scene);
         stage.show();
     }
